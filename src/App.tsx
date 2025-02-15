31a153ac03e28707cc9619f82c5dfdf5dd4800df
koushik ahmed <koushikxahmed@gmail.com>
2025-02-15 13:23:56 +0600

    api endpoint done

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,25 @@ import Layout from "./components/layout";
 import { ThemeProvider } from "./components/context/theme-provider";
 import WeatherDashboard from "./pages/WeatherDashboardPage";
 import CityPage from "./pages/CityPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// Create a client
+const queryClient = new QueryClient();
 
 export default function Home() {
   return (
-    <BrowserRouter>
-      <ThemeProvider defaultTheme="dark">
-        <Layout>
-          <Routes>
-            <Route path="/" element={<WeatherDashboard />} />
-            <Route path="/city/:cityName" element={<CityPage />} />
-          </Routes>
-        </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<WeatherDashboard />} />
+              <Route path="/city/:cityName" element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
